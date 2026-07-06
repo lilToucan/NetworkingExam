c333f8b732445168dc21388646371d7aecaa3ef1
lilToucan <ivancamuzzi@gmail.com>
2026-07-06 14:18:26 +0200

    Restart

--- Source/NetworkingExam/NetworkingExam.cpp
+++
@@ -1,6 +0,0 @@
-// Copyright Epic Games, Inc. All Rights Reserved.
-
-#include "NetworkingExam.h"
-#include "Modules/ModuleManager.h"
-
-IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, NetworkingExam, "NetworkingExam" );
